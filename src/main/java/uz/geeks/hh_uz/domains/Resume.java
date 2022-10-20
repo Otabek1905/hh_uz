170e1550673af9dfff0418e700cfadd7e8ab3d90
otabek <otaw1324@gmail.com>
2022-10-20 18:36:59 +0500

    Init

---
+++ src/main/java/uz/geeks/hh_uz/domains/Resume.java
@@ -0,0 +1,14 @@
+package uz.geeks.hh_uz.domains;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Resume {
+
+}
---
+++ src/test/java/uz/geeks/hh_uz/HhUzApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.geeks.hh_uz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhUzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

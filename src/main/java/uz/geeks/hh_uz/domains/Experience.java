313bf6dced4eccd4d1758013d4dc731c769ed639
jahongir <atametov97@mail.ru>
2022-10-20 18:49:46 +0500

    Experience added

---
+++ src/main/java/uz/geeks/hh_uz/domains/Experience.java
@@ -0,0 +1,14 @@
+package uz.geeks.hh_uz.domains;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Builder
+public class Experience {
+}

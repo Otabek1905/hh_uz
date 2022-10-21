5c6e034f26e14f506e8c809af386daeb236bf679
otabek <otaw1324@gmail.com>
2022-10-21 17:41:22 +0500

    packages

--- src/main/java/uz/geeks/hh_uz/domains/Education.java
+++ src/main/java/uz/geeks/hh_uz/domains/Education.java
@@ -1,4 +1,25 @@
 package uz.geeks.hh_uz.domains;
 
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Education {
+    private String universityName;
+    private String department;
+    private String faculty;
+    private Integer graduationYear;
+    private Level degree;
+
+    enum Level{
+        Bachelor,
+        PhD,
+        Higher,
+        Secondary
+    }
+
 }
--- src/main/java/uz/geeks/hh_uz/domains/Experience.java
+++ src/main/java/uz/geeks/hh_uz/domains/Experience.java
@@ -1,15 +1,18 @@
 package uz.geeks.hh_uz.domains;
 
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
 @Setter
 @ToString
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class Experience {
-
+    private String organization;
+    private String region;
+    private String website;
+    private String position;
+    private String description;
 }
